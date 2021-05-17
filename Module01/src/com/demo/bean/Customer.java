3a158b300265a8e73ba11eddc4683ee47ac00038
Liaojiafeng <645673816@qq.com>
2021-05-17 17:48:24 +0800

    Initial commit

---
+++ Module01/src/com/demo/bean/Customer.java
@@ -0,0 +1,16 @@
+package com.demo.bean;
+
+import java.util.ArrayList;
+
+/*
+快捷键使用
+ */
+public class Customer {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        ArrayList list = new ArrayList();
+
+    }
+
+}
